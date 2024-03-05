989870d35b3891ca2755cc11eddd3e2df7c2b391
Sathishramesh1 <sathishrameshkec@gmail.com>
2024-03-05 20:33:07 +0530

    render-deployment

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import CustomError from "../utils/error.js";
+
+export const protectRoute = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token || !token.startsWith("Bearer ")) {
+    return res.status(404).send('Token not found');
+  }
+
+  const authToken = token.slice(7); 
+
+  try {
+    const decoded = jwt.verify(authToken, process.env.JWT_SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    throw new CustomError("Access denied. Invalid token.", 401);
+  }
+};
