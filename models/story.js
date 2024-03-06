3d2573a8b18d4c128cd9474a888db7a1a9f5961c
Sathishramesh1 <sathishrameshkec@gmail.com>
2024-03-06 12:54:09 +0530

    default-image-update

--- models/story.js
+++ models/story.js
@@ -24,7 +24,7 @@ const storySchema = new mongoose.Schema(
     image: {
       type: String,
       default:
-        "https://thersilentboy.com/wp-content/uploads/2022/09/Blogging.jpeg",
+        "https://img.freepik.com/free-vector/software-code-testing-concept-illustration_114360-8414.jpg?w=740&t=st=1709709779~exp=1709710379~hmac=7bf073be0b0a008ac7fe76336cb6e3f707c1bc9554ef51fc058c1d1a13d3ceda",
     },
   },
   { timestamps: true }
